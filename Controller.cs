46f0da5d8e7fbad71f39deed51b9b2a1e5a3952e
THAI NHAT MINH <1512323@student.hcmus.edu.vn>
2018-05-26 17:04:40 +0700

    C# Script

---
+++ Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    public GameObject owner;
+    public GameObject planer;
+    public float offset;
+	// Use this for initialization
+	void Start () {
+        
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 n = (owner.transform.position - planer.transform.position).normalized;
+        Vector3 pos = n * offset;
+        transform.LookAt(planer.transform.position);
+        transform.position = pos;
+
+    }
+}
---
+++ Gravity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+    public float gravitationalPull;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        //or apply gravity to all game objects with rigidbody
+        foreach (GameObject o in UnityEngine.Object.FindObjectsOfType<GameObject>())
+        {
+            if (o.GetComponent<Rigidbody>() && o != gameObject)
+            {
+                o.GetComponent<Rigidbody>().AddForce((transform.position - o.transform.position).normalized * gravitationalPull);
+            }
+        }
+    }
+}
